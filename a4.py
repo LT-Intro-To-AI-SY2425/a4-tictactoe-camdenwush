bfc29dcf9d3dd7d36c2ab34d872fd6218b06616a
camdenwush <cgrush@cps.edu>
2024-11-06 11:25:03 -0600

    fix freakbob

--- a4.py
+++ a4.py
@@ -60,3 +60,63 @@ class TTTBoard:
             " ".join(self.board[i:i+3]) for i in range(0, 9, 3)
         )
  
+def play_tic_tac_toe() -> None:
+    """Uses your class to play TicTacToe"""
+    def is_int(maybe_int: str):
+        """Returns True if val is int, False otherwise
+        Args:
+            maybe_int - string to check if it's an int
+        Returns:
+            True if maybe_int is an int, False otherwise
+        """
+        try:
+            int(maybe_int)
+            return True
+        except ValueError:
+            return False
+    brd = TTTBoard()
+    players = ["X", "O"]
+    turn = 0
+    while not brd.game_over():
+        print(brd)
+        move: str = input(f"Player {players[turn]} what is your move? ")
+        if not is_int(move):
+            raise ValueError(
+                f"Given invalid position {move}, position must be integer between 0 and 8 inclusive"
+            )
+        if brd.make_move(players[turn], int(move)):
+            turn = not turn
+    print(f"\nGame over!\n\n{brd}")
+    if brd.has_won(players[0]):
+        print(f"{players[0]} wins!")
+    elif brd.has_won(players[1]):
+        print(f"{players[1]} wins!")
+    else:
+        print(f"Board full, cat's game!")
+
+
+if __name__ == "__main__":
+    # here are some tests. These are not at all exhaustive tests. You will DEFINITELY
+    # need to write some more tests to make sure that your TTTBoard class is behaving
+    # properly.
+    brd = TTTBoard()
+    brd.make_move("X", 8)
+    brd.make_move("O", 7)
+    assert brd.game_over() == False
+    brd.make_move("X", 5)
+    brd.make_move("O", 6)
+    brd.make_move("X", 2)
+    assert brd.has_won("X") == True
+    assert brd.has_won("O") == False
+    assert brd.game_over() == True
+    brd.clear()
+    assert brd.game_over() == False
+    brd.make_move("O", 3)
+    brd.make_move("O", 4)
+    brd.make_move("O", 5)
+    assert brd.has_won("X") == False
+    assert brd.has_won("O") == True
+    assert brd.game_over() == True
+    print("All tests passed!")
+    # uncomment to play!
+    # play_tic_tac_toe()
